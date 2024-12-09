7ca7de481d08a5f443b487a249061879ef48a9e2
aglowinthefield <tom.auty+nexus@gmail.com>
2024-12-09 09:34:35 -0500

    Forgot util

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 logger: logging.Logger = logging.getLogger("DownloadManager")
 
+
 def create_logger() -> None:
     """
     Creates a logger with a file handler and sets it to the DEBUG level.
@@ -17,14 +18,16 @@ def create_logger() -> None:
     logs_dir: str = os.path.abspath(os.path.join(script_dir, "..", "..", "logs"))
     Path(logs_dir).mkdir(parents=True, exist_ok=True)
     log_path: str = os.path.abspath(os.path.join(logs_dir, "DownloadManager.log"))
-    with open(log_path, "w") as _:
+    with open(log_path, "w", encoding="utf-8") as _:
         pass
 
     # Create a file handler that logs messages to "logs\PageFileManager.log"
     file_handler = logging.FileHandler(log_path, encoding="utf-8", mode="w")
 
     # Set the format of the log messages
-    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s")
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s"
+    )
     file_handler.setFormatter(formatter)
 
     # Add the file handler to the logger
